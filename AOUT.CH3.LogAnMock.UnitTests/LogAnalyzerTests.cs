07cbeaa0af35843896f4e31b9a78106e0d138349
eugeneborodin <eugeneborodin@mail.ru>
2017-02-06 01:15:17 +0300

    init

---
+++ AOUT.CH3.LogAnMock.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AOUT.CH3.LogAnMock.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void Analyze_TooShortFileName_CallsWebService()
+        {
+            FakeWebService mockService = new FakeWebService();
+            LogAnalyzer log = new LogAnalyzer(mockService);
+            string tooShortFileName = "abc.ext";
+
+            log.Analyze(tooShortFileName);
+
+            StringAssert.Contains("Слишком короткое имя файла: abc.ext",
+                mockService.LastError);
+        }
+    }
+}
---
+++ AOUT.CH3.LogAnMock/LogAnalyzer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOUT.CH3.LogAnMock
+{
+    public interface IWebService
+    {
+        void LogError(string message);
+    }
+
+    public class FakeWebService : IWebService
+    {
+        public string LastError;
+
+        public void LogError(string message)
+        {
+            LastError = message;
+        }
+    }
+
+    public class LogAnalyzer
+    {
+        private IWebService service;
+
+        public LogAnalyzer(IWebService service)
+        {
+            this.service = service;
+        }
+
+        public void Analyze(string fileName)
+        {
+            if (fileName.Length < 8)
+            {
+                service.LogError("Слишком короткое имя файла: " + fileName);
+            }
+        }
+    }
+}
